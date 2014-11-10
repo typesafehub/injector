3889a484ee6831b6847292370335b3dc5996be08
Antonio Cunei <antonio.cunei@typesafe.com>
2014-11-10 18:30:47 +0100

    More validation, and file scanning

--- build.sbt
+++ build.sbt
@@ -4,12 +4,28 @@ version in Global := "0.1"
 
 scalaVersion in Global := "2.11.4"
 
-lazy val injector = project
+lazy val injector = project.settings(sourceGenerators in Compile <+= (sourceManaged in Compile, version, name, streams) map { (dir, v, n, s) =>
+        val file = dir / "Defaults.scala"
+        if(!dir.isDirectory) dir.mkdirs()
+        s.log.info("Generating "+file.getName+" into "+file.getCanonicalPath)
+        IO.write(file, """
+          package com.typesafe.injector
+          object Defaults {
+            val version = "%s"
+            val name = "%s"
+            val hash = "%s"
+          }
+        """ format (v, n, Process("git log --pretty=format:%H -n 1").lines.head))
+        Seq(file)
+      })
 
-libraryDependencies in Global := (libraryDependencies in Global).value ++ Seq(
+libraryDependencies in Global := (libraryDependencies in Global).value ++ {
+  val sbtVer = sbtVersion.value
+  Seq(
   "org.rogach" %% "scallop" % "0.9.5",
-  "org.scala-sbt" % "launcher-interface" % "0.13.6" % "provided"
-)
+  "org.scala-sbt" % "launcher-interface" % sbtVer % "provided",
+  "org.scala-sbt" % "io" % sbtVer
+)}
 
 lazy val root = Project("root", file("."))
   .aggregate(injector)
--- injector/src/main/scala/com/typesafe/injector/Injector.scala
+++ injector/src/main/scala/com/typesafe/injector/Injector.scala
@@ -2,8 +2,16 @@ package com.typesafe.injector
 
 import org.rogach.scallop._
 import org.rogach.scallop.exceptions.ScallopException
+import sbt.PathFinder._
+import sbt.Path._
+import sbt.GlobFilter
+import sbt.DirectoryFilter
+import java.io.File
+import java.util.jar.JarInputStream
+import java.util.jar.JarFile
+import java.util.jar.JarEntry
 
-object InjectorMain { def main(args: Array[String]) = (new Injector).start(args) }
+object InjectorMain { def main(args: Array[String]) = (new Injector).start(args, Defaults.name, Defaults.version) }
 case class Exit(val code: Int) extends xsbti.Exit
 class Injector extends xsbti.AppMain {
   def run(configuration: xsbti.AppConfiguration) = {
@@ -30,24 +38,45 @@ class Injector extends xsbti.AppMain {
                |or multiple times the same option to add elements.
                |For additional information, please contact http://www.typesafe.com.""".stripMargin)
       val debug = opt[Boolean](noshort = true, descr = "Print more debugging information")
+      private def fileExists(p: String) = {
+        val f = new File(p)
+        val valid = f.exists
+        if (!valid) println("This element cannot be found: " + p)
+        valid
+      }
       val files = opt[List[String]](descr = "Path to the file(s) that should be inserted into jars.",
-        required = true)
-      val jars = opt[List[String]](descr = "Patterns that specify which jars should be considered." +
+        required = true, validate = (_ forall fileExists))
+      val jars = opt[List[String]](descr = "Patterns that specify which jars should be considered, in glob format." +
         "For instance, c*.jar will match all jars whose basename begins with c. If multiple patterns are specified, " +
         "all the jars that match at least one pattern will be considered. All patterns must end with \".jar\". " +
         "If omitted, all jars will be processed.",
-        validate = (_.forall(_.endsWith(".jar"))),
+        validate = (_.forall { j =>
+          val valid = j.endsWith(".jar") && !j.contains("/")
+          if (!valid) {
+            println("This pattern contains a forward slash, or does not end with \".jar\": " + j)
+          }
+          valid
+        }),
         default = Some(List[String]("*.jar")))
       val directories = opt[List[String]](required = true, descr = "One or more paths to the directories containing the jars " +
-        "that will be processed. Every directory will be scanned recursively.")
+        "that will be processed. Every directory will be scanned recursively.", validate = (_ forall fileExists))
       val noChecksums = opt[Boolean](descr = "Do not regenerate the checksum files of the modified jar files. By default, " +
         "New mds and sha1 files will be generated, to replace the existing ones.")
     }
     val conf = new conf(name, version)
     val debug = conf.debug()
     val file = conf.files()
-    val jars = conf.jars()
+    val patterns = conf.jars()
     val dirs = conf.directories()
     val checksums = !conf.noChecksums()
+
+    // Let's find out which jars need to be processed
+
+    val filter = patterns.map(GlobFilter(_)).reduce(_ | _).--(DirectoryFilter)
+    val jars = dirs.map(new File(_)).map(_.**(filter)).reduce(_ +++ _).get
+
+    jars foreach { jar =>
+
+    }
   }
 }
